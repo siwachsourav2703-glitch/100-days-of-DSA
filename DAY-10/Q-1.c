7148470d1fe7557c6c06da2c7c042db0eb8b7851
siwachsourav2703-glitch <siwachsourav2703@gmail.com>
2026-02-10 08:50:35 +0530

    commit

---
+++ DAY-10/Q-1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char s[100];
+    int left = 0, right = 0;
+
+    scanf("%s", s);
+
+    // find length
+    while (s[right] != '\0') {
+        right++;
+    }
+    right--;  
+    while (left < right) {
+        if (s[left] != s[right]) {
+            printf("NO");
+            return 0;
+        }
+        left++;
+        right--;
+    }
+    printf("YES");
+    return 0;
+}
