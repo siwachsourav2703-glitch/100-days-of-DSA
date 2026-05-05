e7d6c6705c1b35a2e2aee2a68ebbe15a29f946a6
siwachsourav2703-glitch <siwachsourav2703@gmail.com>
2026-05-05 19:29:49 +0530

    commit

---
+++ DAY-94/Q-1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+void countingSort(int arr[], int n) {
+    int i;
+
+    int max = arr[0];
+    for(i = 1; i < n; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+    }
+
+    int *count = (int *)calloc(max + 1, sizeof(int));
+
+    for(i = 0; i < n; i++) {
+        count[arr[i]]++;
+    }
+
+    for(i = 1; i <= max; i++) {
+        count[i] += count[i - 1];
+    }
+
+    int *output = (int *)malloc(n * sizeof(int));
+    for(i = n - 1; i >= 0; i--) {
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+
+    for(i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+
+    free(count);
+    free(output);
+}
+int main() {
+    int arr[] = {4, 2, 2, 8, 3, 3, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    countingSort(arr, n);
+
+    printf("Sorted array: ");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
